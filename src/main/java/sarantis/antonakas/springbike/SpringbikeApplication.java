f13bad90e85aa0341b16c21ca91c9e82485a32fe
sarandis <sarandis10@hotmail.com>
2021-08-07 10:08:51 +0100

    swagger added

--- src/main/java/sarantis/antonakas/springbike/SpringbikeApplication.java
+++ src/main/java/sarantis/antonakas/springbike/SpringbikeApplication.java
@@ -6,10 +6,13 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
 @ComponentScan("sarantis.antonakas.*")
 @EntityScan("sarantis.antonakas.entity")
 @EnableJpaRepositories("sarantis.antonakas.repository")
+@EnableSwagger2
 public class SpringbikeApplication {
 
 	public static void main(String[] args) {
